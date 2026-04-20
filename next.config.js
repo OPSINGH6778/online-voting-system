356c732223eab72d543bf64ec4dc7894ed364410
OPSINGH6778 <opsingh26122002@gmail.com>
2026-04-21 00:24:21 +0530

    Fix next config for Vercel build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Moved out of experimental as required by Next.js 16+
-  serverExternalPackages: ['mongoose', 'nodemailer'], 
-  
+  serverExternalPackages: ['mongoose', 'nodemailer'],
+
   typescript: {
     ignoreBuildErrors: true,
   },
-  
-  // Removed the unsupported 'eslint' block that was causing warnings
+
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
-  
+
   poweredByHeader: false,
 };
 
